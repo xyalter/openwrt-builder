93add32dfafc8c50037c094b8e634a63799150c8
xxy1991 <xxy1991@gmail.com>
2020-05-02 14:45:28 +0800

    feat(image-builder): add keepalived template

---
+++ image-builder/templates/keepalived/files/etc/uci-defaults/99_keepalived.sh
@@ -0,0 +1,10 @@
+#!/bin/ash
+#
+# Copyright (C) 2019 xxy1991
+#
+
+echo "" >> /etc/sysupgrade.conf
+echo "/etc/keepalived/" >> /etc/sysupgrade.conf
+echo "/etc/conntrackd/" >> /etc/sysupgrade.conf
+
+exit 0
